af3151a2d4a6e91f970604a8d838a2166d249dcd
Adam <adam@adam.pl>
2019-05-13 00:35:59 -0700

    2

---
+++ CMakeFiles/ExampleProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExampleProject.dir/main.cpp.o"
+  "ExampleProject.pdb"
+  "ExampleProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExampleProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/CMakeFiles/ExampleProject_src.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExampleProject_src.dir/GoldenCard.cpp.o"
+  "CMakeFiles/ExampleProject_src.dir/Gracz.cpp.o"
+  "CMakeFiles/ExampleProject_src.dir/wonder.cpp.o"
+  "libExampleProject_src.pdb"
+  "libExampleProject_src.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExampleProject_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
